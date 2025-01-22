02d68f297fc210dcd839f18bac888cc6be04795e
Pragati Gupta <pragatigupta0104@gmail.com>
2025-01-22 12:23:51 +0530

    g

--- toolkit/src/App.jsx
+++ toolkit/src/App.jsx
@@ -1,7 +1,15 @@
+import { useDispatch, useSelector } from "react-redux"
+import {dec,inc} from "./Createslice"
 function App(){
+    let c= useSelector((s)=>s.counter.value)
+    let dis = useDispatch()
+
     return(
         <>
-        <h1>toolkit</h1>
+        <h1>{c}</h1>
+        <button onClick={()=>dis(inc())}>Inc</button>
+        <button onClick={()=>dis(dec())}>Dec</button>
         </>
     )
 }
+export default App
